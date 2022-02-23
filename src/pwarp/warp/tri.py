f6d882930c4465a2511e11917fc94c6088e844e7
mike <mikecokina@gmail.com>
2022-02-23 22:52:24 +0100

    graph defined warp - optimized (it is turbowarp now)

--- src/pwarp/warp/tri.py
+++ src/pwarp/warp/tri.py
@@ -12,8 +12,40 @@ __all__ = (
 )
 
 
-def _broadcast_to_destination(dst: np.ndarray, mask: np.ndarray):
-    pass
+def inbox_tri_warp(
+        src: np.ndarray,
+        tri_src: np.ndarray,
+        tri_dst: np.ndarray,
+        use_scikit: bool = True
+) -> Tuple[np.ndarray, np.ndarray, Tuple[int, int, int, int]]:
+    # Find bounding rectangle for each triangle
+    bbox_src = cv2.boundingRect(tri_src)
+    bbox_dst = cv2.boundingRect(tri_dst)
+
+    # Offset points by left top corner of the respective rectangles.
+    tri_src_cropped = tri_src[0, :, :] - bbox_src[:2]
+    tri_dst_cropped = tri_dst[0, :, :] - bbox_dst[:2]
+
+    # Crop input image.
+    src_cropped = src[bbox_src[1]:bbox_src[1] + bbox_src[3], bbox_src[0]:bbox_src[0] + bbox_src[2]]
+
+    # Given a pair of triangles, find the affine transform.
+    if use_scikit:
+        # Scikit based warp requires inverse approach to source/destination points.
+        scikit_warp_matrix = affine.affine_transformation(tri_dst_cropped, tri_src_cropped)
+        dst_cropped = affine.warp(src_cropped, scikit_warp_matrix, mode='edge', output_shape=(bbox_dst[3], bbox_dst[2]))
+    else:
+        cv2_warp_matrix = affine.affine_transformation(tri_src_cropped, tri_dst_cropped)[:2, :]
+        # Apply the Affine Transform  to the src image.
+        _kwargs = dict(flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT101)
+        dst_cropped = cv2.warpAffine(src_cropped, cv2_warp_matrix, (bbox_dst[2], bbox_dst[3]), None, **_kwargs)
+
+    # Get mask by filling triangle.
+    mask = np.zeros((bbox_dst[3], bbox_dst[2], 3), dtype=dtype.UINT8)
+    cv2.fillConvexPoly(mask, dtype.INT32(tri_dst_cropped), (1, 1, 1), 16, 0)
+    dst_cropped *= mask
+
+    return dst_cropped, mask, bbox_dst
 
 
 def tri_warp(
@@ -45,7 +77,7 @@ def tri_warp(
     src_cropped = src[bbox_src[1]:bbox_src[1] + bbox_src[3], bbox_src[0]:bbox_src[0] + bbox_src[2]]
 
     # Given a pair of triangles, find the affine transform.
-    if use_scikit == "cv2":
+    if use_scikit:
         # Scikit based warp requires inverse approach to source/destination points.
         scikit_warp_matrix = affine.affine_transformation(tri_dst_cropped, tri_src_cropped)
         dst_cropped = affine.warp(src_cropped, scikit_warp_matrix, mode='edge', output_shape=(bbox_dst[3], bbox_dst[2]))
@@ -109,21 +141,39 @@ def graph_defined_warp(
         faces_src: np.ndarray,
         vertices_dst: np.ndarray,
         faces_dst: np.ndarray,
-        use_scikit: Union[dtype.BOOL, bool]
+        use_scikit: Union[dtype.BOOL, bool] = False,
 ) -> np.ndarray:
     """
     Based on triangulated shape transformed from source to destination mesh
     will provide transformation of image. The idea is to do an affine transformation
     of each triangle in mesh. The affine transformation is definde for each triangle
     separately and partial results are merged at the end of the process.
-    """
-    height, width = image.shape[:2]
-    base_image = np.ones((height, width, 3), dtype=np.uint8) * 255
 
+    :param image: np.ndarray; image
+    :param vertices_src: np.ndarray;
+    :param faces_src: np.ndarray;
+    :param vertices_dst: np.ndarray;
+    :param faces_dst: np.ndarray;
+    :param use_scikit: bool;
+    :return: np.ndarray;
+    """
+    # Create base white image.
+    base_image = np.ones(image.shape, dtype=dtype.UINT8) * 255
+
+    # Iterate over all faces.
     for f_src, f_dst in zip(faces_src, faces_dst):
+        # Choose corresponding vertices defined by faces.
         r_src, r_dst = vertices_src[f_src], vertices_dst[f_dst]
-        r_src, r_dst = np.expand_dims(r_src, axis=0), np.expand_dims(r_dst, axis=0)
-        warped, alpha = tri_warp(image, r_src, r_dst, use_scikit=use_scikit)
-        base_image[alpha, :] = warped[alpha, :]
+        r_src, r_dst = np.array([r_src], dtype=r_src.dtype), np.array([r_dst], dtype=r_dst.dtype)
+        # Transform given triangles pair in affine manner.
+        warped, alpha, bbox = inbox_tri_warp(image, r_src, r_dst, use_scikit=use_scikit)
+
+        # Put transformed data within base image based on alpha mask and bounding box of given destination face.
+        # Copy triangular region of the rectangular patch to the output image.
+        base_image[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] = \
+            base_image[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] * ((1.0, 1.0, 1.0) - alpha)
+
+        base_image[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] = \
+            base_image[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] + warped
 
     return base_image
