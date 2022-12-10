b92699e08ff337b29483551b6301d38e851b5012
mike <mikecokina@gmail.com>
2022-12-10 18:16:06 +0100

    Demo loader supports scaling factor as `FLOAT` instead of `INT` what also allows to decrease size of supplied triangular mesh

--- src/pwarp/_io.py
+++ src/pwarp/_io.py
@@ -6,29 +6,39 @@ from pwarp.core import dtype
 
 
 def read_wavefront(path: str):
-    def parse_line(_line):
-        _line = re.findall(r"[-+]?\d*\.*\d+", _line)
+    def parse_line(_line: str):
+        # If line starts with `f` it means, face is defined and
+        # we wanna be able to hande different wavefront forms.
+        # Supported wavefront forms:
+        #   f v1 v2 v3 ....
+        #   f v1/vt1 v2/vt2 v3/vt3 ...
+        #   f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3 ...
+        #   f v1//vn1 v2//vn2 v3//vn3 ...
+        if _line.startswith('f'):
+            return [int(c.split('/')[0]) for c in line.split(' ')[1:]]
+        elif _line.startswith('v'):
+            _line = re.findall(r"[-+]?\d*\.*\d+", _line)
         return np.array(_line, dtype=dtype.FLOAT)
 
     vertices, faces = [], []
 
     with open(path, "r") as file:
         for line in file:
-            if line[0] == "#":
-                # skip comments
+            if line[0] not in ["v", "f"]:
+                # Skip all lines different from vertex/face definition.
                 continue
 
             data = parse_line(line)
             if line[0] == "v":
-                # append vertex
+                # Append vertex (expeceting 2D space, so we ingore 3rd cooridnate).
                 vertices.append(np.array(data[:2], dtype=dtype.FLOAT))
 
             elif line[0] == "f":
-                # append face
+                # Append face.
                 faces.append(np.array(data[:3], dtype=dtype.INDEX))
 
     vertices = np.array(vertices, dtype=dtype.FLOAT)
-    # shift faces by 1 due to convinience in wavefront obj
+    # Shift faces by 1 due to convinience in wavefront obj.
     faces = np.array(faces, dtype=dtype.INDEX) - 1
 
     return len(vertices), len(faces), vertices, faces
