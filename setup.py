66be1da97337f604c99d17544be00269ce942015
mike <mikecokina@gmail.com>
2026-01-15 22:21:37 +0100

    fix(setup): fix setup content to latest version

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-"""
-Pre-release steps
+"""Pre-release steps.
+
 -----------------
 
 New approach:
@@ -58,52 +58,51 @@ New approach:
 """
 import sys
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-from os import path
+
+# Always prefer setuptools over distutils
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 sys.path.insert(0, here)
 
 from version import get_version  # noqa
 
-
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = "For more information visit https://github.com/mikecokina/puppet-warp/blob/master/README.rst"
 
 setup(
-    name='puppet-warp',
-    src_root='src',
+    name="puppet-warp",
+    src_root="src",
     version=get_version(),
 
-    description='Puppet Warp',
+    description="Puppet Warp",
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/mikecokina/puppet-warp',
+    url="https://github.com/mikecokina/puppet-warp",
 
     # Author details
-    author='Michal Cokina',
-    author_email='mikecokina@gmail.com',
+    author="Michal Cokina",
+    author_email="mikecokina@gmail.com",
 
     # Choose your license
-    license='GPL-3.0-or-later',
+    license="GPL-3.0-or-later",
 
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 3 - Alpha",
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Image Processing',
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Image Processing",
 
         # Pick your license as you wish (should match "license" above)
         # 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
@@ -111,21 +110,19 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 
     # What does your project relate to?
-    keywords='image',
+    keywords="image",
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(where='src', exclude=["single_system"]),
-    packages=find_packages(where='src'),
+    packages=find_packages(where="src"),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -136,11 +133,11 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'numpy>=1.21.5',
-        'opencv-contrib-python>=4.5.4.60,<=4.11.0.86',
-        'opencv-python>=4.5.4.60,<=4.11.0.86',
-        'scikit-image>=0.19.2,<=0.25.1',
-        'scikit-learn>=1.0.2,<=1.6.1',
+        "numpy>=1.21.5",
+        "opencv-contrib-python>=4.5.4.60,<=4.12.0.88",
+        "opencv-python>=4.5.4.60,<=4.12.0.88",
+        "scikit-image>=0.19.2,<=0.26.0",
+        "scikit-learn>=1.0.2,<=1.8.0",
     ],
 
     # List additional groups of dependencies here (e.g. development
@@ -148,24 +145,24 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': [],
-        'jrs': ['triangle>=20200424'],
-        'test': ['parameterized>=0.8.1'],
+        "dev": [],
+        "jrs": ["triangle>=20200424"],
+        "test": ["parameterized>=0.8.1"],
     },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'pwarp': [
-            'data/*',
-            'conf/logging_schemas/*'
+        "pwarp": [
+            "data/*",
+            "conf/logging_schemas/*",
         ],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[],
 
@@ -173,7 +170,7 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
         ],
     },
 )
